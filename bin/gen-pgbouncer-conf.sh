3bfa568658abaa6a74d6dc3fb5f3da59ce545082
James King <james@yobota.xyz>
2017-04-06 16:51:27 +0100

    Attempt to remove stunnel

--- bin/gen-pgbouncer-conf.sh
+++ bin/gen-pgbouncer-conf.sh
@@ -81,6 +81,4 @@ cat /app/vendor/pgbouncer/pgbouncer.ini
 echo `date`": /app/vendor/pgbouncer/users.txt"
 cat /app/vendor/pgbouncer/users.txt
 
-mkdir -p /var/run/postgresql
-chmod 700 /var/run/postgresql
-ls -dl /var/run/postgresql
+
