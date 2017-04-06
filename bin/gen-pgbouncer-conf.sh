eeebe928ca9591645270a681dbfa38a91ff45339
James King <james@yobota.xyz>
2017-04-06 17:44:29 +0100

    Please work

--- bin/gen-pgbouncer-conf.sh
+++ bin/gen-pgbouncer-conf.sh
@@ -65,7 +65,8 @@ do
 EOFEOF
 
   cat >> /app/vendor/pgbouncer/pgbouncer.ini << EOFEOF
-$CLIENT_DB_NAME= dbname=$DB_NAME port=610${n}
+$CLIENT_DB_NAME= host=$DB_HOST port=$DB_PORT dbname=$DB_NAME
+
 EOFEOF
 
   let "n += 1"
